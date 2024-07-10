adc19be5d012299c73b9f406a395e7c19b70f07a
Isaac Lyons <isaac@snowskeleton.net>
2024-07-10 14:37:34 +0000

    move, don't copy

--- install.sh
+++ install.sh
@@ -9,8 +9,7 @@ sudo systemctl disable kumad
 sudo rm -rf /usr/local/bin/kumad
 
 #install fresh copy
-sudo cp kumad /usr/local/bin/kumad
-# sudo mv kumad /usr/local/bin/kumad
+sudo mv kumad /usr/local/bin/kumad
 sudo touch /etc/kumad.yaml
 echo "Download complete!"
 echo 'Start with '

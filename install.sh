8806a1be3a644ab59a0316476fe291d42d6ddbe4
Isaac Lyons <isaac@snowskeleton.net>
2024-07-10 14:16:55 +0000

    install from repo

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# download latest binary
+curl -O https://github.com/snowskeleton/kumad/blob/main/kumad
+
 # clean up old installs
 sudo systemctl stop kumad
 sudo systemctl disable kumad
 sudo rm -rf /usr/local/bin/kumad
 
 #install fresh copy
-sudo cp kumad /usr/local/bin/kumad
+sudo mv kumad /usr/local/bin/kumad
 sudo touch /etc/kumad.yaml
+echo "Download complete!"
+echo 'Start with '
+echo -e "\tsudo kumad up --push_url '<Uptime Kuma URL>'"
